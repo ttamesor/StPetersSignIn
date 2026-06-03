a4026b373e29c27c9083e359bef59b82e5b13e54
ttamesor <125159879+ttamesor@users.noreply.github.com>
2026-06-03 14:27:51 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbzOfNBbBtcPntLsOWBhrqJBMIEVtU9yM1kh7ISoQzcYgNY543L0yZSTCCOvUDwWAYUFxQ/exec",
+  ADMIN_PASSWORD_HASH: "db4317528fbc98220c371f29ce21d907836377ca0cc00d497829c3025f5a7bc3"
+};
